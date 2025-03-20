434a93394c7f3120591fb02afdfb300f9cbfae32
Sumit sahoo <sahoosumit811@gmail.com>
2025-03-20 16:00:36 +0000

    variable

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,13 @@
+// alert(3+3)  We are using node js , it is applicable for browser.
+"use strict"; //treat all js code as newer version
+
+
+console.log(3+3)
+
+let name = "sumit sahoo"  //string dataType
+let age = 19  //number dataType
+let isLoggedIn = false  //boolean dataType
+let state = null  //null dataType
+
+console.log(typeof name, age)
+console.log(typeof(name, age))
---
+++ 01_basics/variable.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "sahoosumit811@gmail.com"
+var accountPassword = "12345"
+accountCity = "kolkata" 
+let accountState;
+
+// accountId = 2  Not allowed bcz accountid is defined as constant in the 1st line.
+
+accountEmail = "sumitsahoo6545@gmail.com"
+accountPassword = "234246"
+accountCity = "Chennai"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
