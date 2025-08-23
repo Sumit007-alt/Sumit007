b726e504822103b85d40705993c553ff3f20f1c9
Sumit sahoo <sahoosumit811@gmail.com>
2025-08-23 17:09:25 +0000

    23/08/2025  e done

---
+++ 02_basics/destructure.js
@@ -0,0 +1,13 @@
+const course = {
+    coursename : "JS in Hindi",
+    price : "999",
+    instructor : "Chai aur code"
+}
+
+console.log(course.instructor);
+
+const {instructor:ins} = course
+console.log(ins);   
+
+// From instructure to ins... this is called destructure in object. means to do minization. 
+
