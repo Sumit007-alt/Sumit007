da6e0f6a1a4eba2e2fba628283889b9f3bd05e48
Sumit sahoo <sahoosumit811@gmail.com>
2025-08-24 16:58:35 +0000

    24/08/2025 e done

---
+++ 03_basics/01_function.js
@@ -0,0 +1,24 @@
+function sayMyName(){
+    console.log("S");
+    console.log("U");
+    console.log("M");
+    console.log("I");
+    console.log("T");
+    
+}
+
+// sayMyName()
+
+function addTwoNumber(number1, number2){
+    // let result = (number1+number2);
+    // return result
+
+    return (number1+number2);
+}
+// const result = addTwoNumber(3,5)
+const result = addTwoNumber(4,"a")
+// console.log(`result:${result}`);
+
+
+
+
